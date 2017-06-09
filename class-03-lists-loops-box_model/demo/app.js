7c64f48942d91210b25146939d0daa00fb385e4a
Sam Hamm <samhamm@gmail.com>
2017-06-09 10:48:45 -0700

    middle of class 6/9

--- class-03-lists-loops-box_model/demo/app.js
+++ class-03-lists-loops-box_model/demo/app.js
@@ -1,26 +1,27 @@
 'use strict';
 
-// var guessesLeft = 4;
-// var secretNumber = 8;
-//
-// while(guessesLeft > 0) {
-//   var answer = parseInt(prompt('What is the secret number? Hint: It is in the range of 1 and 10'));
-//   if (answer === secretNumber) {
-//     alert('YOU GOT IT!');
-//     guessesLeft = -1;
-//   } else if (answer < secretNumber) {
-//     guessesLeft--;
-//     alert('Too low. You have ' + guessesLeft + ' guesses left.');
-//   } else if (answer > secretNumber) {
-//     guessesLeft--;
-//     alert('Too high. You have ' + guessesLeft + ' guesses left.');
-//   }
-//   if (guessesLeft === 0) {
-//     alert('You suck at guessing. The correct number is 8');
-//   }
-// }
+var guessesLeft = 4;
+var secretNumber = 8;
 
-var states = ['Tennessee', 'New York', 'Louisiana', 'Montana'];
+while(guessesLeft > 0) {
+  var answer = parseInt(prompt('What is the secret number? Hint: It is in the range of 1 and 10'));
+  if (answer === secretNumber) {
+    alert('YOU GOT IT!');
+    guessesLeft = -1;
+  } else if (answer < secretNumber) {
+    guessesLeft--;
+    alert('Too low. You have ' + guessesLeft + ' guesses left.');
+  } else if (answer > secretNumber) {
+    guessesLeft--;
+    alert('Too high. You have ' + guessesLeft + ' guesses left.');
+  }
+  if (guessesLeft === 0) {
+    alert('You suck at guessing. The correct number is 8');
+  }
+}
+
+var states = ['Tennessee', 'New York', 'Louisiana'];
+var stateAbbr = ['TN', 'NY', 'LA'];
 var stateGuesses = 6;
 
 while(stateGuesses > 0) {
@@ -28,7 +29,7 @@ while(stateGuesses > 0) {
   var stateAnswer = prompt('Name a state that I have lived in besides Washington, Montana, Alabama, or Florida).');
 
   for(var i = 0; i < states.length; i++) {
-    if(stateAnswer === states[i]){
+    if(stateAnswer === states[i] || stateAnswer === stateAbbr[i]){
       alert('Nice! You got one!');
       stateGuesses = -1;
       break;
